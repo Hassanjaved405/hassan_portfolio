884eb55d51ffe1e1fd29f61bb3a239dde9ff987a
Hassanjaved405 <hassanjaved405@gmail.com>
2026-03-05 20:05:40 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
       <Navigation />
       <Hero />
       <About />
-      <Experience />
       <Skills />
+      {/* <Experience /> */}
       <Projects />
       <Contact />
       <footer>
